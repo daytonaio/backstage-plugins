c951a34d6e6323efdde2cb0f4a2dad72b6c70fba
adityasinghal26 <adiaggarwal26@gmail.com>
2024-09-21 09:51:56 +0000

    fix: remove unnecessary logging
    Signed-off-by: adityasinghal26 <adiaggarwal26@gmail.com>

--- plugins/daytona/src/components/WorkspaceOverviewContent/WorkspaceOverviewCard.tsx
+++ plugins/daytona/src/components/WorkspaceOverviewContent/WorkspaceOverviewCard.tsx
@@ -10,28 +10,20 @@ export const WorkspaceOverviewCard = () => {
       // Initialize state based on session storage
       return window.sessionStorage.getItem('isDaytonaSignedIn') === 'true';
     });
-    // window.sessionStorage.setItem('isDaytonaSignedIn', 'false');
-    // const isDaytonaSignedIn = window.sessionStorage.getItem('isDaytonaSignedIn') == 'true';
 
     useEffect(() => {
       const authSubscription = daytonaAuthApi.sessionState$().subscribe(state => {
-        console.log('isDaytonaSignedIn inside useEffect', state);
         if (state === SessionState.SignedIn) {
           setIsLoggedIn(true);
           window.sessionStorage.setItem('isDaytonaSignedIn', 'true');
-          console.log('isDaytonaSignedIn inside if-block', window.sessionStorage.getItem('isDaytonaSignedIn') == 'true');
         } else {
           setIsLoggedIn(false);
-          window.sessionStorage.setItem('isDaytonaSignedIn', 'false');
         }
       });
       return () => {
         authSubscription.unsubscribe();
       };
     }, [daytonaAuthApi]);
-
-    // const isDaytonaSignedIn = window.sessionStorage.getItem('isDaytonaSignedIn') == 'true';
-    console.log('isDaytonaSignedIn after useEffect', window.sessionStorage.getItem('isDaytonaSignedIn') == 'true');
     
     return (
       <>
