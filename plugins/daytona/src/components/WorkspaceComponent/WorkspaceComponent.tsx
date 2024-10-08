159dee47f1b548dfb73759346b5b5156e19878e1
adityasinghal26 <adiaggarwal26@gmail.com>
2024-10-08 19:40:37 +0000

    feat: add tab loading based on user admin privilege
    Signed-off-by: Aditya Singhal <adiaggarwal26@gmail.com>

--- plugins/daytona/src/components/WorkspaceComponent/WorkspaceComponent.tsx
+++ plugins/daytona/src/components/WorkspaceComponent/WorkspaceComponent.tsx
@@ -9,12 +9,10 @@ import {
 import { WorkspaceListCard } from '../WorkspaceListComponent';
 import { AdminCardComponent } from '../AdminCardComponent';
 
-export const WorkspaceComponent = () => (
-  <Page themeId="tool">
-    <Header title="Welcome to Daytona!">
-      <HeaderLabel label="Owner" value="Daytona" />
-      <HeaderLabel label="Lifecycle" value="Alpha" />
-    </Header>
+export const WorkspaceComponentContent = () => {
+  const isAdmin = true
+
+  const adminContent = (
     <TabbedLayout>
       <TabbedLayout.Route path='workspaces' title="Workspaces">
           <Grid container spacing={3} direction="column">
@@ -31,5 +29,33 @@ export const WorkspaceComponent = () => (
           </Grid>
       </TabbedLayout.Route>
     </TabbedLayout>
+  );
+
+  const nonAdminContent = (
+    <TabbedLayout>
+      <TabbedLayout.Route path='workspaces' title="Workspaces">
+          <Grid container spacing={3} direction="column">
+            <Grid item>
+              <WorkspaceListCard />
+            </Grid>
+          </Grid>
+      </TabbedLayout.Route>
+    </TabbedLayout>
+  );
+
+  return (
+    <>
+      { isAdmin ? adminContent : nonAdminContent }
+    </>
+  )
+}
+
+export const WorkspaceComponent = () => (
+  <Page themeId="tool">
+    <Header title="Welcome to Daytona!">
+      <HeaderLabel label="Owner" value="Daytona" />
+      <HeaderLabel label="Lifecycle" value="Alpha" />
+    </Header>
+    <WorkspaceComponentContent />
   </Page>
 );
